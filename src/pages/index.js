bba587999a1cf7e45b898624fd423af6d132b7da
camo55 <car25ram@gmail.com>
2024-03-23 00:47:57 +0000

    commit1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,40 @@
-import { Jumbotron } from "@/components/Jumbotron";
-import { Button, Col, Container, Row } from "react-bootstrap";
+// src/pages/index.js
+import { Login } from '@/components/Login';
+import React from 'react';
 
-
-export default function Home() {
+function App() {
   return (
-    <div>
-      <Container>
-        <Row>
-          <Col>
-            <h1>My Own React Template</h1>
-            <Button variant="primary">Click Here</Button>
-          </Col>
-        </Row>
-      </Container>
-      <Container>
-        <Jumbotron />
-      </Container>
+    <div className="App">
+      <header className="App-header">
+        <h1>Login</h1>
+        <Login />
+      </header>
     </div>
   );
 }
+
+export default App;
+
+
+
+// import { Jumbotron } from "@/components/Jumbotron";
+// import { Button, Col, Container, Row } from "react-bootstrap";
+
+
+// export default function Home() {
+//   return (
+//     <div>
+//       <Container>
+//         <Row>
+//           <Col>
+//             <h1>My Own React Template</h1>
+//             <Button variant="primary">Click Here</Button>
+//           </Col>
+//         </Row>
+//       </Container>
+//       <Container>
+//         <Jumbotron />
+//       </Container>
+//     </div>
+//   );
+// }
